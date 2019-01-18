22443e5980761194b942d3c285bbe462d7daff72
Jeremiah Njoroge <jnjoroge@gmail.com>
2019-01-18 09:39:24 -0800

    Bitcode support

--- lib/curl_builder/compiler.rb
+++ lib/curl_builder/compiler.rb
@@ -102,6 +102,10 @@ module CurlBuilder
         min_version = "-mmacosx-version-min=10.7"
       end
 
+      if setup(:bitcode)
+        embed_bitcode = "-fembed-bitcode"
+      end
+      
       sdk_version = sdk_version_for platform
       sdk = "#{setup(:xcode_home)}/Platforms/#{platform}.platform/Developer/SDKs/#{platform}#{sdk_version}.sdk"
 
